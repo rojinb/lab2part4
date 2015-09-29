9b0c33bdbc31836a16d252575e50a688d312b8b2
rojin <rojinb25@hotmail.com>
2015-09-29 16:30:07 -0700

    added conflicting change

--- src/arch/sm213/machine/student/MainMemory.java
+++ src/arch/sm213/machine/student/MainMemory.java
@@ -40,10 +40,7 @@ public class MainMemory extends AbstractMainMemory {
    */
   @Override public int bytesToInteger (byte byteAtAddrPlus0, byte byteAtAddrPlus1, byte byteAtAddrPlus2, byte byteAtAddrPlus3) {
 
-	  return (((int) byteAtAddrPlus0) & 0xff) << 24 |
-	           (((int) byteAtAddrPlus1) & 0xff) << 16 |
-	           (((int) byteAtAddrPlus2) & 0xff) <<  8 |
-	           (((int) byteAtAddrPlus3) & 0xff);
+	  return 2;
   }
   
   /**
